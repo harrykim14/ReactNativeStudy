345afe2c70c5caf3006d56e79784110d0cfad1ba
harrykim14 <harrykim14@korea.ac.kr>
2020-09-17 15:28:42 +0900

    Studying ReactNative through MovieApp at 20.09.14~17
    Chapter 8 (Code Review Finished)

--- MovieApp/src/Context/User/index.tsx
+++ MovieApp/src/Context/User/index.tsx
@@ -16,20 +16,24 @@ const UserContext = createContext(defaultContext);
 interface Props {
     children: JSX.Element | Array<JSX.Element>;
 }
-// Props에서는 
+// JSX가 가지고 있는 Element를 children으로 받아 Props화
 
 const UserContextProvider = ({children}: Props) =>{
+    // @types/index.d.ts에 지정된 모든 Element를 가져와서 사용하기 때문에 IUserInfo를 사용할 수 있다
     const [userInfo, setUserInfo] = useState<IUserInfo | undefined>(undefined);
     const [isLoading, setIsLoading] = useState<boolean>(false);
 
     const login = (email:string, password:string):void => {
+        // Static void login(String email, String password){} 와 같은 것
         AsyncStorage.setItem('token', 'save your token')
+        // AsyncStorage의 setItem은 Promise를 return하기 때문에 then, catch등을 이용할 수 있다
         .then(()=>{
             setUserInfo({
                 name: 'dev-yakusza',
                 email: 'dev.yakuza@gmail.com',
             });
             setIsLoading(true);
+            // useState내에 있는 isLoding값을 true로 설정
         })
     };
 
@@ -52,6 +56,7 @@ const UserContextProvider = ({children}: Props) =>{
     
     const logout = ():void =>{
         AsyncStorage.removeItem('token');
+        // AsyncStorage.setItem('token', 'save your token')로 저장한 토큰값을 삭제하여 로그아웃 처리
         setUserInfo(undefined);
     }
 
