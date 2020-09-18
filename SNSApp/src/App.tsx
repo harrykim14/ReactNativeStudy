20746dbf2a664861f019f9609a00f952ef59a972
harrykim14 <harrykim14@korea.ac.kr>
2020-09-18 18:50:36 +0900

    Studying ReactNative through SNSApp at 20.09.17~18
    chapter 9 (Screen developing)

--- SNSApp/src/App.tsx
+++ SNSApp/src/App.tsx
@@ -1,14 +1,24 @@
 
 import React from 'react';
-import Styled from 'styled-components/native';
+import {StatusBar} from 'react-native';
 
-const App = () => {
+import Navigator from '~/Screens/Navigator';
+import {UserContextProvider} from '~/Context/User';
+import {RandomUserDataProvider} from '~/Context/RandomUserData';
+
+interface Props {}
+
+const App = ({}: Props) => {
   return (
     <>
-      
+      <RandomUserDataProvider cache={true}>
+        <UserContextProvider>
+          <StatusBar barStyle="default"/>
+          <Navigator/>
+        </UserContextProvider>
+      </RandomUserDataProvider>
     </>
   );
 };
-
 
 export default App;
