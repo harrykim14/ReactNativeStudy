b4fa47b334935b230a8d44e1818ecfd2decabbc3
harrykim14 <harrykim14@korea.ac.kr>
2020-09-23 16:09:27 +0900

    Studying ReactNative through Portfolio App 20.09.22~23
    Check personal react-native study by creating an App (Day 2nd:
    Add navigator codes)

---
+++ PortfolioApp/src/Screens/Navigator.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import { UserContext } from '~/Context/User';
+import Loading from '~/Components/Loading';
+
+import Profile from '~/Screens/Profile';
+import Project from '~/Screens/Project';
+
+const Stack = createStackNavigator();
+const BottomTab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
