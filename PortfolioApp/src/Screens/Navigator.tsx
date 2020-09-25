e1a087bc45239c7a0927e101bf48d4c9a5eb458e
harrykim14 <harrykim14@korea.ac.kr>
2020-09-25 16:11:31 +0900

    Studying ReactNative through Portfolio App 20.09.22~25
    Check personal react-native study by creating an App (Day 4th:
    Add navigator codes)

--- PortfolioApp/src/Screens/Navigator.tsx
+++ PortfolioApp/src/Screens/Navigator.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import { UserContext } from '~/Context/User';
 import Loading from '~/Components/Loading';
-
+import PortfolioMain from '~/Components/PortfolioMain';
 import Profile from '~/Screens/Profile';
 import Project from '~/Screens/Project';
 
@@ -15,14 +14,65 @@ const Stack = createStackNavigator();
 const BottomTab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
 
-const MyProfile = () => {
-    return(
+interface ILoadingContext {
+    isLoading: boolean;
+}
+
+const MainPage = () => {
+    return (
         <Stack.Navigator>
             <Stack.Screen
+            name = "Portfolio"
+            component = {PortfolioMain}
+            options ={{
+                title: 'Portfolio',
+                headerTintColor: '#87CEEB',
+                headerStyle : {
+                    backgroundColor : '#f4f4f4',
+                    borderBottomWidth : 0,
+                },
+                headerTitleStyle: {
+                    fontWeight: 'bold'
+                }
+            }}
+            />
+            <Stack.Screen
             name = "Profile"
-            component = {Profile}
-            options={{ title: 'Profile'}} />
+            component={Profile}
+            options={{
+                title: 'Profile',
+                headerStyle : {
+                    backgroundColor : '#f4f4f4',
+                    borderBottomWidth : 0,
+                },
+                headerTitleStyle: {
+                    fontWeight: 'bold'
+                }
+            }}
+            />
+            <Stack.Screen
+            name = "Project"
+            component = {Project}
+            options = {{
+                title:'Project',
+                headerStyle : {
+                    backgroundColor : '#f4f4f4',
+                    borderBottomWidth : 0,
+                },
+                headerTitleStyle: {
+                    fontWeight: 'bold'
+                }
+            }}
+            />
         </Stack.Navigator>
     )
 }
 
+
+export default () => {
+    const { isLoading } = useContext<ILoadingContext>()
+    if(isLoading === false) {
+        return <Loading />
+    }
+
+}
